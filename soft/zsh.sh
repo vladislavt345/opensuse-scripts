93d5b28dd93d77c2d3d724bd34a267b8c4a0fd2b
vladislavt345 <vladislavt30515@gmail.com>
2025-11-17 19:51:58 +0300

    -

--- soft/zsh.sh
+++ soft/zsh.sh
@@ -2,12 +2,11 @@ sudo zypper install zsh git
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 zed ~/.zshrc
-plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
+sed -i 's/^plugins=.*/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
 source ~/.zshrc
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 sudo reboot
-# check
 echo $SHELL
